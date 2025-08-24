ba743dc5d4f9c6b8e8c7fe7a85012fc3b4d38406
User <user@example.com>
2025-08-24 13:46:18 +0000

    feat: update project configuration for Vercel deployment

--- alx-movie-app/next.config.js
+++ alx-movie-app/next.config.js
@@ -1,14 +1,18 @@
 /** @type {import('next').NextConfig} */
-const withPWA = require('@ducanh2912/next-pwa').default({
+const withPWA = require('@ducanh2912/next-pwa')({
   dest: 'public',
   disable: process.env.NODE_ENV === 'development',
   register: true,
   skipWaiting: true,
+  // Disable PWA in development
+  buildExcludes: [/middleware-manifest.json$/]
 });
 
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  // Enable static exports for the app to be deployed as a static website
+  output: 'standalone',
 };
 
 module.exports = withPWA(nextConfig);
